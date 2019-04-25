7c795d6de6c36281ebd366f9bfc9a263f58209f9
TNMR-m <49636252+TNMR-m@users.noreply.github.com>
2019-04-25 16:49:43 +0900

    更新

--- 09.py
+++ 09.py
@@ -3,7 +3,7 @@ def kansu09(str_x):
 
     a = 0
 
-    import random                         # "random"モジュールをインポート（？）
+    from random import shuffle                         # シャッフルをインポート
 
     for i in str_x:
         str1 = str_x[a]                   # 現在扱っている単語をstr1とする
@@ -12,7 +12,7 @@ def kansu09(str_x):
         if mojisu > 4:                    # ！　単語の文字数が4文字より多い時、最初と最後以外の文字をシャッフル
             nakami1 = str1[1:-1]          # 単語の2～最後から一つ手前の文字までを取り出す
             nakami2 = list(nakami1)       # ↑ を一文字ずつに区切ってリストにする
-            random.shuffle(nakami2)       # 中身をシャッフル
+            shuffle(nakami2)       # 中身をシャッフル
             nakami1 = ''.join(nakami2)    # 中身を文字列に戻す
             b = str1[:1] + nakami1 + str1[-1:]  # 最初＆最後の文字とくっつける
             str_x[a] = b                  # 元のリストの単語を生成した文字列で置き換える
