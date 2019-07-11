b028863577d2302b6d96c91f18de33e4510bf55d
TNMR-m <49636252+TNMR-m@users.noreply.github.com>
2019-07-11 18:26:32 +0900

    20
    実行するとprintが"�"で途切れているのですがあっているのでしょうか…

--- 20.py
+++ 20.py
@@ -1,27 +1,13 @@
-import gzip
+from gzip import open as g_open
 import json
-import re
 
-f1_list = []
-
-with gzip.open('jawiki-country.json.gz', 'r') as f1:
-    for f1_line in f1:
-        f1_column = json.loads(f1_line)
-        f1_list.append(f1_column)
-
-f2_date = ''.join(str(f1_list))
-
-'''
-with open('test_json.txt', 'w', encoding = 'utf-8') as f2:
-    f2.write(f2_date)
-    # 辞書型の活用、と改行の変換？……
-'''
-
-# content = r'test_json.txt'
-content = f2_date
-pattern = 'イギリス'
-
-repatter = re.compile(pattern)
-result = repatter.match(content)
-
-print(result)
+def art_uk(file_name):
+    with g_open(file_name, 'rt', encoding = 'utf-8') as f0:
+        for f0_line in f0:
+            f0_dict = json.loads(f0_line)
+            if f0_dict['title'] == 'イギリス':
+                article_uk = f0_dict['text']
+    
+    return article_uk
+            
+print(art_uk('jawiki-country.json.gz'))
